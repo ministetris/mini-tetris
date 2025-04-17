c3bf5d39726dda33b62ac186388afc7f14ab2f66
MiniK <mjkang.ctech@gmail.com>
2025-04-18 04:06:20 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ if ('serviceWorker' in navigator) {
 
 const bgm = new Audio('bgm_loop.wav');
 bgm.loop = true;
-bgm.volume = 0.3;
+bgm.volume = 0.4;
+
 window.addEventListener('click', () => {
-  if (bgm.paused) bgm.play();
+  if (bgm.paused) {
+    bgm.play().catch((e) => {
+      console.warn("BGM play blocked:", e);
+    });
+  }
 });
