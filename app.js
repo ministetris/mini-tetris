bc83a11e56206223bef171483b7f1f554d5c15a1
MiniK <mjkang.ctech@gmail.com>
2025-04-19 07:59:32 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-
-const bgm = new Audio('bgm_loop.wav');
-bgm.loop = true;
-bgm.volume = 0.4;
-window.addEventListener('click', () => {
-  if (bgm.paused) {
-    bgm.play().catch(() => {});
-  }
-});
+const bgm = new Audio('bgm_loop.wav');bgm.loop = true;bgm.volume = 0.4;window.addEventListener('click', () => {if (bgm.paused) {bgm.play().catch(() => {});}});
